b4828b8f86f0b3ab5b797f665b1763521dbd15e3
Marcus Asplund <marcus@greatname.se>
2023-01-28 17:48:25 +0100

    add error handling

--- app.py
+++ app.py
@@ -12,32 +12,49 @@ output_file = 'output.csv'
 # Google Geolocation API Key
 api_key = 'YOUR_API_KEY'
 
-# Read input CSV file
-with open(input_file, 'r') as input_csv:
-    reader = csv.DictReader(input_csv)
-    data = [row for row in reader]
+try:
+    # Read input CSV file
+    with open(input_file, 'r') as input_csv:
+        reader = csv.DictReader(input_csv)
+        data = [row for row in reader]
+except FileNotFoundError:
+    print(f"Error: Input file '{input_file}' not found.")
+    exit()
 
-# Write output CSV file
-with open(output_file, 'w', newline='') as output_csv:
-    fieldnames = ['city', 'municipality', 'lat', 'lng']
-    writer = csv.DictWriter(output_csv, fieldnames=fieldnames)
-    writer.writeheader()
+try:
+    # Write output CSV file
+    with open(output_file, 'w', newline='') as output_csv:
+        fieldnames = ['city', 'municipality', 'lat', 'lng']
+        writer = csv.DictWriter(output_csv, fieldnames=fieldnames)
+        writer.writeheader()
 
-    for row in data:
-        city = row['city']
-        municipality = row['municipality']
+        for row in data:
+            city = row['city']
+            municipality = row['municipality']
 
-        # Query Google Geolocation API
-        url = f'https://maps.googleapis.com/maps/api/geocode/json?address={city,municipality}&key={api_key}'
-        response = requests.get(url).json()
+            # Query Google Geolocation API
+            url = f'https://maps.googleapis.com/maps/api/geocode/json?address={city}&key={api_key}'
+            response = requests.get(url)
+            if response.status_code != 200:
+                print(f"Error: API returned error code {response.status_code}")
+                continue
+            response = response.json()
 
-        # Extract lat and lng from API response
-        lat = response['results'][0]['geometry']['location']['lat']
-        lng = response['results'][0]['geometry']['location']['lng']
+            if 'error_message' in response:
+                print(f"Error: {response['error_message']}")
+                continue
 
-        # Write to output CSV file
-        writer.writerow({'city': city, 'municipality': municipality, 'lat': lat, 'lng': lng})
-        
-        # Add random interval between 1-3 seconds
-        sleep_time = random.uniform(1, 3)
-        time.sleep(sleep_time)
+            # Extract lat and lng from API response
+            lat = response['results'][0]['geometry']['location']['lat']
+            lng = response['results'][0]['geometry']['location']['lng']
+
+            # Write to output CSV file
+            writer.writerow({'city': city, 'municipality': municipality, 'lat': lat, 'lng': lng})
+
+            # Add random interval between 1-3 seconds
+            sleep_time = random.uniform(1, 3)
+            time.sleep(sleep_time)
+
+except Exception as e:
+    print(f"Error: {e}")
+
